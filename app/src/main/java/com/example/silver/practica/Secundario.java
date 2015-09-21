8a15ccd7ef51bfa245fa6c50776442ff6b4eef72
kevinabel <kevin.ticlavilca@hotmail.com>
2015-09-21 12:15:43 -0500

    Initial commit

---
+++ app/src/main/java/com/example/silver/practica/Secundario.java
@@ -0,0 +1,19 @@
+package com.example.silver.practica;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by SILVER on 21/09/2015.
+ */
+public class Secundario extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registrar);
+        }
+    }
